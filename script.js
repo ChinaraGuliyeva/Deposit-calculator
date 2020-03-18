335aa727ba901b800fa21dec26c1855e855fd46b
Chinara Guliyeva <chinarai@yandex.ru>
2020-03-18 11:56:25 +0400

    edited

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let initialSum=document.getElementById('initial');
+let monthlyPayment=document.getElementById('monthly');
 let button = document.getElementById('calculate');
 button.addEventListener('click', calculateSum);
 
 function calculateSum(){
-    alert('one');
+   console.log(initialSum.value);
+   console.log(monthlyPayment.value);
 }
