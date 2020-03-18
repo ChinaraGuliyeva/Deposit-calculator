d4bcff1bf01baa86c80119083e228f5223272d0d
Chinara Guliyeva <chinarai@yandex.ru>
2020-03-18 11:27:11 +0400

    edited

---
+++ script.js
@@ -0,0 +1,7 @@
+let initialSum=document.getElementById('initial');
+let button = document.getElementById('calculate');
+button.addEventListener('click', calculateSum);
+
+function calculateSum(){
+    alert('one');
+}
