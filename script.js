ad7aee8de3cf5e368eef20b18fd647a0e8e6fb61
ChinaraGuliyeva <chinarai@yandex.ru>
2020-03-19 00:36:38 +0400

    edited

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-let initial = document.getElementById('initial');
-let monthly = document.getElementById('monthly');
-let percent = document.getElementById('percent');
-let deposit = document.getElementById('time');
+let initial = +document.getElementById('initial').value;
+let monthly = +document.getElementById('monthly').value;
+let percent = +document.getElementById('percent').value;
+let deposit = +document.getElementById('time').value;
+let error = document.getElementById('error');
 let button = document.getElementById('calculate');
-button.addEventListener('click', calculateSum);
+button.addEventListener('click', check);
 
-function calculateSum() {
-   let initialSum = +initial.value;
-   //   console.log('Изначальная сумма ' + initialSum);
-   let monthlyPayment = +monthly.value;
-   //   console.log('Ежемесячный платеж ' + monthlyPayment);
-   let percentRate = +percent.value;
-   //   console.log('Процентная ставка ' + percentRate);
-   let depositTime = +deposit.value;
-   //   console.log('Время депозита ' + depositTime);   
+function check() {
+   if (initial > 0 && monthly >= 0 && percent < 100 && percent > 0 && deposit > 0 && Number.isInteger(deposit)) {
+      calculateSum(initial, monthly, percent, deposit);
+   }
+   if (initial <= 0 || isNaN(initial)) {
+      console.log('Ошибка в первой строке');
+      error.insertAdjacentHTML('beforeend', '<p>Ошибка в первой строке</p>');
+   }
+   if (monthly < 0 || isNaN(monthly)) {
+      console.log('Ошибка во второй строке');
+      error.insertAdjacentHTML('beforeend', '<p>Ошибка во второй строке</p>');
+   }
+   if (percent <= 0 || isNaN(percent)) {
+      console.log('Ошибка в третьей строке');
+      error.insertAdjacentHTML('beforeend', '<p>Ошибка в третьей строке</p>');
+   }
+   if (deposit <= 0 || isNaN(deposit) || !Number.isInteger(deposit)) {
+      console.log('Ошибка в четвертой строке');
+      error.insertAdjacentHTML('beforeend', '<p>Ошибка в четвертой строке</p>');
+   }
+}
 
-   if (initialSum > 0 && monthlyPayment >= 0 && percentRate < 100 && percentRate > 0 && depositTime > 0 && Number.isInteger(depositTime)) {
-      let balance = initialSum;
-      let timeMonth = Math.floor(depositTime / 30);
-      //    console.log('Месяцев ' + timeMonth);
-      for (let i = 0; i < timeMonth; i++) {
-         balance = balance + monthlyPayment + (balance + monthlyPayment) * percentRate / 1200;
-      }
-      alert('Баланс будет ' + balance);
+function calculateSum(initial, monthly, percent, deposit) {
+   let balance = initial;
+   let time = Math.floor(deposit / 30);
+   //    console.log('Месяцев ' + timeMonth);
+   for (let i = 0; i < time; i++) {
+      balance = balance + monthly + (balance + monthly) * percent / 1200;
    }
-   else {
-      console.log('Неверный формат');
-      button.insertAdjacentHTML('afterend', '<p>Неверный формат</p>');
-   }
+   alert('Баланс будет ' + balance);
 }
